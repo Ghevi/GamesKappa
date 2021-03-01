d82f96c34853962ccea6fca4ed844d8cda87fe6a
Ghevi <ghevi96@gmail.com>
2021-03-01 23:54:40 +0100

    Not done much, tried to get the console handle to position correctly but failed

--- DigitalBurglar/DigitalBurglar.cpp
+++ DigitalBurglar/DigitalBurglar.cpp
@@ -3,73 +3,73 @@
 
 void PlayGameAtDifficulty(int difficulty)
 {
-
 }
+
 
 int main()
 {
-	int lockDigitsCount = 3;
-	int lockDigitsSum = 8;
-	int lockDigitsProduct = 18;
+	int LockDigitsCount = 3;
+	int LockDigitsSum = 8;
+	int LockDigitsProduct = 18;
 
 #pragma region Initial story lines
 	// Need a collection for items that the player will unlock.
-	std::cout << "While wandering around in the night, you find a fancy hypercar you like." << std::endl;
+	std::cout << "While wandering around in the night, you find a fancy hypercar you like.\n";
 	///
 		// Instead of this ugly if else, in the future locks should be represented by classes (base lock, easy lock, medium lock, hard lock etc...).
-	if (lockDigitsCount > 3)
-		std::cout << "\"Well, a lock with " << lockDigitsCount << " numbers, it gets interesting.\"" << std::endl;
+	if (LockDigitsCount > 3)
+		std::cout << "\"Well, a lock with " << LockDigitsCount << " numbers, it gets interesting.\"\n";
 	else
-		std::cout << "\"Tsk, another one of those stupid security mechanisms with just " << lockDigitsCount << " numbers!\"" << std::endl;
+		std::cout << "\"Tsk, another one of those stupid security mechanisms with just " << LockDigitsCount << " numbers!\"\n";
 
 	// Add a timer on higher levels.
-	std::cout << "The small screen on the car door is waiting for the code..." << std::endl;
-	std::cout << std::endl;
+	std::cout << "The small screen on the car door is waiting for the code...\n";
+	std::cout << "\n";
 	// Maybe in some level the neuralink could not work properly or at all, in which case there should be another way to find the code.
-	std::cout << "Your neural link will give you suggestion to find the right code:" << std::endl;
-	std::cout << std::endl;
-	std::cout << "+ The " << lockDigitsCount << " numbers add up to " << lockDigitsSum << ". \n";
-	std::cout << "+ The " << lockDigitsCount << " numbers multiplication result is " << lockDigitsProduct << "." << std::endl;
-	std::cout << std::endl;
+	std::cout << "Your neural link will give you suggestion to find the right code:\n";
+	std::cout << "\n";
+	std::cout << "+ The " << LockDigitsCount << " numbers add up to " << LockDigitsSum << ". \n";
+	std::cout << "+ The " << LockDigitsCount << " numbers multiplication result is " << LockDigitsProduct << ".\n" ;
+	std::cout << "\n";
 #pragma endregion
 
 #pragma region Lock Screen
-	std::string displayedDigits;
-	for (int i = 0; i < lockDigitsCount; i++)
-		displayedDigits += " # ";
+	std::string DisplayedDigits;
+	for (int i = 0; i < LockDigitsCount; i++)
+		DisplayedDigits += " # ";
 
-	//HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	//COORD position = {displayedDigits.at(1)};
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD position = {0, 10};
+	SetConsoleCursorPosition(hConsole, position);
 
-	for (int i = 0; i <= (lockDigitsCount * 3) + 1; i++)
+	for (int i = 0; i <= (LockDigitsCount * 3) + 1; i++)
 		std::cout << "*";
 
-	std::cout << std::endl;
-	std::cout << "*"; std::cout << displayedDigits; std::cout << "*";
-	std::cout << std::endl;
+	std::cout << "\n";
+	std::cout << "*"; std::cout << DisplayedDigits; std::cout << "*";
+	std::cout << "\n";
 
-	//SetConsoleCursorPosition(hConsole, position);
 
-	for (int i = 0; i <= (lockDigitsCount * 3) + 1; i++)
+	for (int i = 0; i <= (LockDigitsCount * 3) + 1; i++)
 		std::cout << "*";
-	std::cout << std::endl;
-	std::cout << std::endl;
+	std::cout << "\n";
+	std::cout << "\n";
 #pragma endregion
 
 #pragma region Player input
-	int playerGuess1, playerGuess2, playerGuess3;
-	std::cin >> playerGuess1; std::cin >> playerGuess2; std::cin >> playerGuess3;
-	int playerSum = playerGuess1 + playerGuess2 + playerGuess3;
-	int playerProduct = playerGuess1 * playerGuess2 * playerGuess3;
+	int PlayerGuess1, PlayerGuess2, PlayerGuess3;
+	std::cin >> PlayerGuess1; std::cin >> PlayerGuess2; std::cin >> PlayerGuess3;
+	int PlayerSum = PlayerGuess1 + PlayerGuess2 + PlayerGuess3;
+	int PlayerProduct = PlayerGuess1 * PlayerGuess2 * PlayerGuess3;
 #pragma endregion
 
 #pragma region Ending story lines
-	std::cout << std::endl;
-	std::cout << "You enter the code..." << playerGuess1 << playerGuess2 << playerGuess3 << std::endl;
-	if (playerSum == lockDigitsSum && playerProduct == lockDigitsProduct)
-		std::cout << "The code is correct... -\"Damn, imagine spending so much for a car that has this kind of security\"" << "\n";
+	std::cout << "\n";
+	std::cout << "You enter the code..." << PlayerGuess1 << PlayerGuess2 << PlayerGuess3 + "\n";
+	if (PlayerSum == LockDigitsSum && PlayerProduct == LockDigitsProduct)
+		std::cout << "The code is correct... -\"Damn, imagine spending so much for a car that has this kind of security\"\n";
 	else
-		std::cout << "The code is incorrect and the police has been called... \"Well, I guess I'm dumber than the mechanism itself\"" << "\n";
+		std::cout << "The code is incorrect and the police has been called... \"Well, I guess I'm dumber than the mechanism itself\"\n";
 #pragma endregion
 
 	return 0;
